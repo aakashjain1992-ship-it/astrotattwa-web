a7d485ab41e5be57ec54b3cf18462bb83bbe05a1
Aakash Jain <224555312+aakashjain1992-ship-it@users.noreply.github.com>
2026-01-22 18:52:08 +0000

    Production build fix + PM2 ecosystem config

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,22 +1,23 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import * as LabelPrimitive from '@radix-ui/react-label'
-import { cn } from '@/lib/utils'
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
 
-const Label = React.forwardRef
+import { cn } from "@/lib/utils"
+
+const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>>(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
     className={cn(
-      'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+      "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
       className
     )}
     {...props}
   />
 ))
-
 Label.displayName = LabelPrimitive.Root.displayName
 
 export { Label }
