c9c0e277e52d8f5556b60a2064518f24a04f1dc6
Aakash Jain <224555312+aakashjain1992-ship-it@users.noreply.github.com>
2026-03-19 17:27:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ const securityHeaders = [
 ];
 
 const corsHeaders = [
-  { key: 'Access-Control-Allow-Origin', value: 'https://astrotattwa.com' },
+  { key: 'Access-Control-Allow-Origin', value:  process.env.NEXT_PUBLIC_APP_URL || 'https://astrotattwa.com' },
   { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
   { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
 /**  { key: 'Access-Control-Max-Age', value: '86400' },   ## this is disabled now, since we are testing on production only. When we launch the product and have QA set up, then we will enable this for production */
